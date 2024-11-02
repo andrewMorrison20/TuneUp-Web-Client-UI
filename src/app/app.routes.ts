29ce5db7d08499a8569073c50b62190219defa91
andrewMorrison20 <amorrison20@qub.ac.uk>
2024-11-02 09:59:52 +0000

    Adding basic login and home pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,26 +3,13 @@ import { HomeComponent } from './home/home.component';
 import { DetailsComponent } from './details/details.component';
 export const routes: Routes = [
   {
-      path: 'home',
-      component: HomeComponent,
-      title: 'Home page'
-    },
-    {
-      path: 'details/:id',
-      component: DetailsComponent,
-      title: 'Home details'
-    },
-      {
-      path: 'login',
-      component: DetailsComponent,
-      title: 'Home details'
-    },
-  {
     path: 'home2',
     loadChildren: () => import('./home/home.module').then(m => m.HomeModule),
     title: 'Home details'
   },
-
+  { path: 'login',
+    loadChildren: () => import('./login/login.module').then(m => m.LoginModule),
+    title: 'login form'},
   ];
 
 export default routes;
