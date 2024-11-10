ac4db7143359a48ca144b5572e3f0e6ecfb64594
andrewMorrison20 <amorrison20@qub.ac.uk>
2024-11-10 00:01:05 +0000

    refactored nav bar and footer, added search bar

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent {
+  // Add any logic or properties related to the navbar
+  toggleMenu() {
+    // Example of a method to toggle the menu
+  }
+}
---
+++ src/app/components/search-bar/search-bar.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+
+import { SearchBarComponent } from './search-bar.component';
+import {MatOption, MatSelect} from "@angular/material/select";
+
+@NgModule({
+  declarations: [SearchBarComponent],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    FormsModule,
+    MatSelect,
+    MatOption,
+    // Import FormsModule to use ngModel for two-way binding
+  ],
+  exports: [SearchBarComponent]  // Export the SearchBarComponent so it can be used in other modules
+})
+export class SearchBarModule { }
