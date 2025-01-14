2994a44ac5c72d9200961021433caa9c3ee5d953
andrewMorrison20 <amorrison20@qub.ac.uk>
2025-01-14 17:07:58 +0000

    circular dependency

--- src/app/authentication/auth.interceptor.ts
+++ src/app/authentication/auth.interceptor.ts
@@ -1,20 +1,25 @@
 import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http';
 import {AuthenticatedUser} from "./authenticated-user.class";
 import {Injectable} from "@angular/core";
-import {AuthService} from "./auth.service";
+import {JwtHelperService} from "@auth0/angular-jwt";
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private authService: AuthService) {}
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     const token = AuthenticatedUser.getAuthUserToken();
     console.log('Getting auth token for username: ', AuthenticatedUser.getAuthenticatedUser());
-    if (token && this.authService.tokenIsValid()) {
-      const cloned = req.clone({
-        headers: req.headers.set('Authorization', `Bearer ${token}`),
-      });
-      return next.handle(cloned);
+    if (token) {
+      const jwtHelper = new JwtHelperService();
+      if (!jwtHelper.isTokenExpired(token)) {
+        const cloned = req.clone({
+          headers: req.headers.set('Authorization', `Bearer ${token}`),
+        });
+        return next.handle(cloned);
+      } else {
+        console.warn('Token is expired and will not be attached.');
+      }
     }
     return next.handle(req); // Proceed without token
   }
 }
+
