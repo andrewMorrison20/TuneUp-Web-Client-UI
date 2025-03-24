5d9fe1622b4e7cec772eb7b85d4a2bb700bf5a3e
andrewMorrison20 <amorrison20@qub.ac.uk>
2025-03-24 08:52:28 +0000

    adjust routes

--- src/app/notification-link.pipe.ts
+++ src/app/notification-link.pipe.ts
@@ -10,15 +10,15 @@ export class NotificationLinkPipe implements PipeTransform {
       case 'NEW_CHAT':
         return '/user-dashboard/chats';
       case 'LESSON_REQUEST':
-        return '/user-dashboard/lessons';
+        return '/user-dashboard/my-tuitions';
       case 'LESSON_CANCEL':
-        return '/user-dashboard/lessons';
+        return '/user-dashboard/my-tuitions';
       case 'PAYMENT_OVERDUE':
       case 'PAYMENT_MADE':
         return '/user-dashboard/payments';
       case 'REQUEST_ACCEPTED':
       case 'REQUEST_REJECTED':
-        return '/user-dashboard/requests';
+        return '/user-dashboard/my-tuitions';
       default:
         return '/user-dashboard';
     }
