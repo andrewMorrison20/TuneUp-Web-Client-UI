ef3ab3ee5e6b595d0eae36c361f159462158fdc3
andrewMorrison20 <amorrison20@qub.ac.uk>
2025-04-07 18:57:28 +0100

    style

--- src/app/components/footer/footer.module.ts
+++ src/app/components/footer/footer.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {FooterComponent} from "./footer.component";
 import {SharedModule} from "../../shared/shared.module";
+import {RouterLink, RouterLinkActive} from "@angular/router";
 
 
 
@@ -10,6 +11,8 @@ import {SharedModule} from "../../shared/shared.module";
   imports: [
     CommonModule,
     SharedModule,
+    RouterLink,
+    RouterLinkActive,
   ],
   exports:[
     FooterComponent
